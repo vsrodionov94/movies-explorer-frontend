7f5ed4de4336a8c1fc0a69a7fe463743b8bd7b83
Vladimir <vsrodionov94@ya.ru>
2021-05-29 15:53:55 +0700

    add about block

---
+++ src/components/AboutProject/AboutProject.js
@@ -0,0 +1,31 @@
+import React from 'react';
+import './AboutProject.css';
+
+function AboutProject() {
+  return (
+    <div className="about">
+      <h2 className="about__title">О проекте</h2>
+      <div className="about__text-block">
+        <h3 className="about__sub-title">Дипломный проект включал 5 этапов</h3>
+        <h3 className="about__text">Составление плана, работу над бэкендом, вёрстку, добавление функциональности и финальные доработки.</h3>
+      </div>
+      <div className="about__text-block">
+        <h3 className="about__sub-title">На выполнение диплома ушло 5 недель</h3>
+        <h3 className="about__text">У каждого этапа был мягкий и жёсткий дедлайн, которые нужно было соблюдать, чтобы успешно защититься.</h3>
+      </div>
+      <div className="about__progress">
+        <div className="about__progress-backend">
+          <p className="about__progress-backend-bar"> 1 неделя </p>
+          <p className="about__progress-caption"> Back-end </p>
+        </div>
+        <div className="about__progress-frontend">
+          <p className="about__progress-frontend-bar"> 4 недели </p>
+          <p className="about__progress-caption"> Front-end </p>
+        </div>
+      </div>
+
+    </div>
+  )
+}
+
+export default AboutProject;
--- src/components/Main/Main.js
+++ src/components/Main/Main.js
@@ -1,9 +1,11 @@
 import Promo from './../Promo/Promo';
+import AboutProject from './../AboutProject/AboutProject';
 
 function Main(){
   return (
     <div className = "main">
       <Promo />
+      <AboutProject />
     </div>
   )
 }
