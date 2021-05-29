3544a01f8d296493d8195860f904834989fd35a6
Vladimir <vsrodionov94@ya.ru>
2021-05-29 16:37:12 +0700

    add aboutMe component

--- src/components/Main/Main.js
+++ src/components/Main/Main.js
@@ -1,6 +1,7 @@
 import Promo from './../Promo/Promo';
 import AboutProject from './../AboutProject/AboutProject';
 import Techs from './../Techs/Techs';
+import AboutMe from './../AboutMe/AboutMe';
 
 function Main(){
   return (
@@ -8,6 +9,7 @@ function Main(){
       <Promo />
       <AboutProject />
       <Techs />
+      <AboutMe />
     </div>
   )
 }
