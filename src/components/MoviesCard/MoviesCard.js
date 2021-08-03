72a72d6d4afbde48d46036ed60d659bbf9a82278
Vladimir Rodionov <vsrodionov94@yandex.ru>
2021-08-03 17:31:40 +0700

    add card btn logic

--- src/components/MoviesCard/MoviesCard.js
+++ src/components/MoviesCard/MoviesCard.js
@@ -1,5 +1,6 @@
 import './MoviesCard.css';
 import photo1 from "./../../images/card-1.png";
+import { Route } from 'react-router-dom';
 
 export default function MoviesCard({card}) {
   return (
@@ -7,7 +8,12 @@ export default function MoviesCard({card}) {
       <div className="card__caption">
         <h3 className="card__title">33 слова о дизайне</h3>
         <p className="card__time">1ч 42м</p>
-        <button className="card__like"> Like </button>
+        <Route path="/movies">
+          <button className="card__btn" />
+        </Route>
+        <Route path="/saved-movies">
+          <button className="card__btn card__btn_delete" />
+        </Route>
       </div>
       <img className="card__photo" src={photo1} alt="33 слова о дизайне" />
     </li>
