ee37c0ff4abf75015135c16e9bbdbde9e9fe2099
Vladimir <vsrodionov94@ya.ru>
2021-08-07 21:57:05 +0700

    last fix

--- src/components/Profile/Profile.js
+++ src/components/Profile/Profile.js
@@ -1,19 +1,19 @@
-import './Profile.css';
 import Navigation from './../Navigation/Navigation';
+import './Profile.css';
 
 const Profile = () => {
   return(
     <section className="profile">
-    <Navigation />
-      <h1 className="profile__title"> Привет, Виталий!</h1>
+      <Navigation />
+      <h1 className="profile__title">Привет, Виталий!</h1>
       <ul className="profile__list">
         <li className="profile__element"> 
-          <p className="profile__name-element"> Имя </p>
-          <p className="profile__value-element"> Виталий </p>
+          <p className="profile__name-element">Имя</p>
+          <p className="profile__value-element">Виталий</p>
         </li>
         <li className="profile__element"> 
-          <p className="profile__name-element"> E-mail </p>
-          <p className="profile__value-element"> pochta@yandex.ru </p>
+          <p className="profile__name-element">E-mail</p>
+          <p className="profile__value-element">pochta@yandex.ru</p>
         </li>
       </ul>
       <button className="profile__btn">Редактировать</button>
