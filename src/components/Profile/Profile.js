e46794e190a8c33101d55b3d81b2dc3f830dcaae
Vladimir Rodionov <vsrodionov94@yandex.ru>
2021-08-12 18:01:37 +0700

    start add profile inputs

--- src/components/Profile/Profile.js
+++ src/components/Profile/Profile.js
@@ -1,27 +1,61 @@
-import { useContext } from "react";
+import { useContext, useState } from "react";
 import { CurrentUserContext } from "../../contexts/CurrentUserContext";
+import { useFormWithValidation } from "../../utils/validator";
 import Navigation from "../Navigation/Navigation";
 
 import './Profile.css';
 
-const Profile = ({ logOut }) => {
+const Profile = ({ logOut, handleUpdateUser }) => {
   const currentUser = useContext(CurrentUserContext);
-  console.log(currentUser)
+
+  const { values, handleChange, errors, isValid } = useFormWithValidation();
+  let { name, email } = values;
+  name = currentUser.name;
+  email = currentUser.email;
+
+  const classBtn = isValid && (values.name !== currentUser.name || values.email !== currentUser.name) ? 'profile__btn' : 'profile__btn profile__btn_disabled';
+
+  const handleSubmit = e => {
+    if (isValid) {
+      e.preventDefault();
+      const { name, email, password } = values;
+      handleUpdateUser(name, email, password)
+    }
+  }
   return(
     <section className="profile">
       <Navigation />
       <h1 className="profile__title">Привет, {currentUser.name}!</h1>
-      <ul className="profile__list">
-        <li className="profile__element"> 
-          <p className="profile__name-element">Имя</p>
-          <p className="profile__value-element">{currentUser.name}</p>
-        </li>
-        <li className="profile__element"> 
-          <p className="profile__name-element">E-mail</p>
-          <p className="profile__value-element">{currentUser.email}</p>
-        </li>
-      </ul>
-      <button className="profile__btn">Редактировать</button>
+      <form className="profile__form" onSubmit={ handleSubmit }>
+        <ul className="profile__list">
+          <li className="profile__element"> 
+            <p className="profile__name-element">Имя</p>
+            <input 
+              type="text" 
+              required 
+              minLength="2" 
+              maxLength="30" 
+              value={ values.name } 
+              className="profile__value-element" 
+              placeholder='Введите имя'
+              onChange={ handleChange }>
+            </input>
+          </li>
+          <li className="profile__element"> 
+            <p className="profile__name-element">E-mail</p>
+            <input 
+              type="text" 
+              required minLength="2" 
+              maxLength="30" 
+              value={ values.email } 
+              className="profile__value-element"
+              placeholder='Введите email'
+              onChange={ handleChange }>
+            </input>
+          </li>
+        </ul>
+        <button className={classBtn}>Редактировать</button>
+      </form>
       <button className="profile__btn profile__btn_red" onClick={logOut}>Выйти из аккаунта</button>
     </section>
   );
